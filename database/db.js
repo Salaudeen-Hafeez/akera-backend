defe3b25b968e6b8075fa72ef93ad8d1ee9d798b
Salaudeen Hafeez <hafeezsala22@gmail.com>
2022-06-01 15:24:50 -0400

    working on the models and API

--- database/db.js
+++ database/db.js
@@ -111,7 +111,7 @@ const postParcel = (packageData) => {
         _weight
         ) VALUES (
           $1, $2, $3, $4,
-          $5, $6, $7, $8, $9
+          $5, $6, $7, $8, $9, $10, $11
           ) RETURNING *`,
     packageData
   );
