0cbabdc0dcc5c56b2ea5e38a3a314bdaf313d2d5
Salaudeen Hafeez <hafeezsala22@gmail.com>
2022-06-01 15:43:15 -0400

    working on the models and API

--- routes/postroutes.js
+++ routes/postroutes.js
@@ -127,6 +127,7 @@ postRouter.post(
         reqBody['tracking_id'] = uuidv4();
         const packageData = Object.values(req.body);
         packageData.push('Ready for pickup');
+        res.json({data: packageData})
         const newPackage = await model.postParcel(packageData);
         if (newPackage.rowCount === 1) {
           const userPackage = await model.getUserParcels(username)
