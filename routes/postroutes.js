0d6a354c7259eb848ed5476f8ef63acbc9d6e96d
Salaudeen Hafeez <hafeezsala22@gmail.com>
2022-06-01 05:43:46 -0400

    working on the models and API

--- routes/postroutes.js
+++ routes/postroutes.js
@@ -115,12 +115,12 @@ postRouter.post(
   async (req, res) => {
     const { username } = req.params;
     const reqBody = req.body;
-    res.json(req.body)
     if (reqBody.frajile === '') {
       reqBody['frajile'] = 'package not frajile';
     }
     const { error } = parcelValidation(reqBody); // Validate the incoming package data
     if (error) {
+      res.json(req.body)
       throw new Error(error.details[0].message);
     } else {
       try {
