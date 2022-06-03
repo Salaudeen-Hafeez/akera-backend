7aa29c34f96b726261cecb1d92d6d0107a1ca457
Salaudeen Hafeez <hafeezsala22@gmail.com>
2022-06-03 19:36:00 -0400

    Working on the add parcel

--- database/db.js
+++ database/db.js
@@ -98,17 +98,17 @@ const postAdmin = (adminData) => {
 const postParcel = (packageData) => {
   const newPackage = client.query(
     `INSERT INTO parcels (
-        _status,
+        _weight
         _name,
         _location,
         _destination,
         _sender,
         _reciever,
         _frajile,
-        _username,
         _cost,
+        _username,
         tracking_id,
-        _weight
+        _status
         ) VALUES (
           $1, $2, $3, $4,
           $5, $6, $7, $8, $9, $10, $11
