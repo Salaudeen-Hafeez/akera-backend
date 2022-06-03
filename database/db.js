64103253758b8b6bacbe6dd344e0982d4d22496a
Salaudeen Hafeez <hafeezsala22@gmail.com>
2022-06-03 19:48:22 -0400

    Working on the add parcel

--- database/db.js
+++ database/db.js
@@ -108,7 +108,7 @@ const postParcel = (packageData) => {
         _reciever,
         _status,
         _username,
-        tracking_id,
+        tracking_id
         ) VALUES (
           $1, $2, $3, $4,
           $5, $6, $7, $8, $9, $10, $11
