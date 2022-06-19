145d2eb8f4b84aaf3df1b4c718130f5667de4cd3
Salaudeen Hafeez <hafeezsala22@gmail.com>
2022-06-19 17:21:43 -0400

    working on the swgger documentation

--- app.js
+++ app.js
@@ -46,22 +46,7 @@ app.set('view engine', 'ejs');
 // The middleware for the home page
 app.get('/', (req, res) => {
   res.send(
-    <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="/style.css" />
-    <script src="./jsmodules/routAuth.js"></script>
-    <title>user profile</title>
-  </head>
-  <body>
-    <h2>'Welcom to Akera logistics API'</h2>
-      <a href="https://akera-backend.herokuapp.com/api/v1/api-docs">
-        API Documentation
-      </a>
-  </body>
-</html>
+  '<h2>Welcom to Akera logistics API</h2>'
   );
 });
 
